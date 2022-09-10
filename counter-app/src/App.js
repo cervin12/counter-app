ec5e7747927497314ea468e972f53076e053c5f2
Cervin <houcervin55@gmail.com>
2022-09-10 07:17:40 +0700

    add counter and reset button

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
-      <h1>0</h1>
-      <button>+</button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={()=>setCount(count*0)}>Reset</button>
     </div>
   );
 }
